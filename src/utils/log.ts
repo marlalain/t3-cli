b09f42d21563edaa67e225c78cf6adf322ade88a
Alex <alex@alexaa.dev>
2022-09-17 08:52:19 -0300

    feat: logging

---
+++ src/utils/log.ts
@@ -0,0 +1,19 @@
+// noinspection JSUnusedGlobalSymbols
+
+import chalk from 'chalk';
+
+export const log = (...e: string[]) => {
+	console.log(chalk.blue(e));
+};
+
+export const warn = (...e: string[]) => {
+	console.warn(chalk.yellow(e));
+};
+
+export const error = (...e: string[]) => {
+	console.error(chalk.red(e));
+};
+
+export const success = (...e: string[]) => {
+	console.log(chalk.green(e));
+};
