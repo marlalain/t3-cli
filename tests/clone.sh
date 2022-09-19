3307568a240ae6f1d1d0791255064eeac383e92c
Alex <alex@alexaa.dev>
2022-09-19 13:13:58 -0300

    fix(tests/clone): `cjs` -> `js`

--- tests/clone.sh
+++ tests/clone.sh
@@ -6,4 +6,4 @@ cd /tmp
 rm -rf tmp-app 1>/dev/null
 git clone https://github.com/minsk-dev/create-t3-app-template tmp-app 1>/dev/null 2>&1
 cd tmp-app
-node "$TOOLS_DIR"/dist/index.cjs
+node "$TOOLS_DIR"/dist/index.js
