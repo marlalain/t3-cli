47969ddc40d054282c5f4af05b618ccc865e58bd
Alex <alex@alexaa.dev>
2022-09-20 05:39:54 -0300

    test: tests against `calcom/cal.com` now

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 TOOLS_DIR=$(pwd)
-npm run build 1>/dev/null
-cd /tmp/tmp-app || exit 1
-node "$TOOLS_DIR"/dist/index.js
+npm run build 1>/dev/null || exit 1
+cd ~/playground/cal.com || exit 1
+node "$TOOLS_DIR"/dist/index.js migrate
